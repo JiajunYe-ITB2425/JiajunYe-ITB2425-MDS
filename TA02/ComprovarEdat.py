889d6eca04958743d3cac1479c225a840a170723
Jiajun Ye <jiajun.ye.7e6@itb.cat>
2024-09-26 08:43:31 +0200

    prova

---
+++ TA02/ComprovarEdat.py
@@ -0,0 +1,13 @@
+"""
+Jiajun Ye
+27/09/2024
+ASIXc MDS TA02
+Descripció: Programa que demana l'edat i diu si ets major d'edat.
+"""
+
+edat = int(input("Quina edat tens?"))
+if edat >= 18:
+    print("Ets major d'edat")
+else:
+    print("Ets menor d'edat")
+print("Programa Finalitzat")
